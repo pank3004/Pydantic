ade111ae2fabc31cefbe6065862a4a3def317172
pank3004 <pankajkumawatew3004@gmail.com>
2025-06-09 10:31:41 +0530

    updated

--- 2_Pydantic_basic.py
+++ 2_Pydantic_basic.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 
-class Patient(BaseModel):    # Even though you didn't write a constructor (__init__()), the BaseModel from Pydantic automatically creates one behind the scenes — and it includes type validation, parsing, and error handling.
+# step 1 define pydantic class
+         # include fields, types, validation constaints(eg gt=0)
+         # Even though you didn't write a constructor (__init__()), the BaseModel from Pydantic automatically creates one behind the scenes — and it includes type validation, parsing, and error handling.
+class Patient(BaseModel):    
     name:str 
     age:int
 
@@ -11,8 +14,11 @@ def insert_patient(patient:Patient):
 
 
 record={'name':'pankaj', 'age':'thirty'}
-
+# step 2 instantiate the class with row input data(usually dictonary or json)
+         # pydantic autumatic validate (if data doest't meet model's req then raise validation error)
 patient1=Patient(**record)
 
+# step 3 pass the validated object to the function
+         # ensoures every part of program works clean, type-safe and logically valid data
 insert_patient(patient1)
 
